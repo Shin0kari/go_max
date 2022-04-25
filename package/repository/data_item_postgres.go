a1ed3233acf96a9af46e8be305108798eb1cdd50
Shinokari <kasper13122@mail.ru>
2022-04-25 12:53:08 +0300

    little fix

--- package/repository/data_item_postgres.go
+++ package/repository/data_item_postgres.go
@@ -67,7 +67,7 @@ func (r *DataItemPostgres) GetById(userId, itemId int) (serv.DataItem, error) {
 }
 
 func (r *DataItemPostgres) Delete(userId, itemId int) error {
-	query := fmt.Sprintf("DELETE FROM %s ti USING %s ti, %s il WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $1 AND ti.id = $2",
+	query := fmt.Sprintf("DELETE FROM %s ti USING %s li, %s ul WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $1 AND ti.id = $2",
 		dataItemsTable, listsItemsTable, usersListsTable)
 	_, err := r.db.Exec(query, userId, itemId)
 	return err
